50c2a9a7af6ddad3fdcc15647f0ee64e0d1b1c5c
paccao <16489683+paccao@users.noreply.github.com>
2021-11-29 22:49:26 +0100

    styled the navbar and made it sticky based on scroll position on the page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,85 @@
 import NavLink from "./NavLink"
-import NavIcon from "./Cart"
 import Search from "./Search"
 import Bookmarks from "./Bookmarks"
 import Cart from "./Cart"
+import styled from "styled-components"
+import { useRef, useEffect } from "react"
 
 export default function Navbar() {
+  const navWrapper = useRef(null)
+  let sticky
+
+  useEffect(() => {
+    // window.scrollY is always how far the top of your screen is from the top of the document (which is 0)
+    if (window.scrollY === 0) {
+      // getBoundingClientRect is the distance from the element to the scrollY
+      sticky = navWrapper.current.getBoundingClientRect().top
+    } else {
+      // If we dont start at the top the page, calculate the difference (delta) from the top
+      const topDelta = navWrapper.current.getBoundingClientRect().top
+      // Get the actual position where we have currently scrolled to
+      const actualDelta = topDelta + window.scrollY
+      sticky = actualDelta
+    }
+  }, [navWrapper])
+
+  window.onscroll = function () {
+    stickyHandlerFunction()
+  }
+
+  function stickyHandlerFunction() {
+    if (window.scrollY >= sticky) {
+      navWrapper.current.classList.add("sticky")
+    } else {
+      navWrapper.current.classList.remove("sticky")
+    }
+  }
+
   return (
-    <nav>
-      <ul>
-        {/* Link tags */}
-        <NavLink title={"Home"} href={"/"} />
-        <NavLink title={"Products"} href={"#products"} />
-        <NavLink title={"Collections"} href={"#collections"} />
-        <Search />
-        <Bookmarks />
-        <Cart />
-        {/* Search icon */}
-        {/* Bookmarks */}
-        {/* Cart */}
+    <Wrapper ref={navWrapper}>
+      <nav className="left-nav">
+        <ul className="navbar-ul">
+          {/* Link tags */}
+          <NavLink title={"Home"} href={"/"} />
+          <NavLink title={"Products"} href={"#products"} />
+          <NavLink title={"Collections"} href={"#collections"} />
+        </ul>
+      </nav>
+      <h2>Taco</h2>
+      <nav className="right-nav">
+        <ul className="navbar-ul">
+          <Search />
+          <Bookmarks />
+          <Cart />
+        </ul>
         {/* if(!loggedin) CTA button  */}
         {/* "login" // else Account icon */}
-      </ul>
-    </nav>
+      </nav>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  scroll-behavior: smooth;
+  display: grid;
+  grid-template-columns: 1fr max-content 1fr;
+  overflow: hidden;
+  .left-nav {
+    width: 33%;
+  }
+
+  .navbar-ul {
+    display: flex;
+    justify-content: space-between;
+    min-width: max-content;
+  }
+
+  .right-nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .sticky + .content {
+  }
+`
